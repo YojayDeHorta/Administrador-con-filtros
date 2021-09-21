260db2305313dcbfd0ee1bf27af2a513c235136d
Ander_AAM <ander_2030@hotmail.com>
2021-09-21 15:35:57 -0500

    inicio - registro - boceto

---
+++ src/components/Tabs_Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="Tabs">
+        <ul class="nav nav-pills mb-3 d-flex justify-center mt-5" id="pills-tab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Login</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Sign Up</button>
+            </li>
+        </ul>
+        <div class="tab-content d-flex justify-center" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab"><Login></Login></div>
+            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab"><Sign></Sign></div>
+         
+        </div>
+    </div>
+</template>
+<script>
+import Login from '../components/Login.vue';
+	import Sign from '../components/Registro.vue';
+export default {
+	name:'Tabs',
+	components:{
+		Login,Sign
+	}
+}
+</script>
+<style scoped>
+.Tabs{
+	border:5px solid red;
+	margin:auto;
+}
+.nav li{
+		margin-left: 25px;
+	}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,17 @@
 <template>
-  <v-container>
-    <h1>esta es la pagina del login</h1> 
-    <v-btn :to="{name: 'Options'}" exact text class="primary"><span>Iniciar sesion</span></v-btn>
-        
-    
-  </v-container>
+    <v-container>
+        <h1>esta es la pagina del login</h1>
+        <Tabs></Tabs>
+        <v-btn :to="{name: 'Options'}" exact text class="primary"><span>Iniciar sesion</span></v-btn>
+    </v-container>
 </template>
-
 <script>
-
-
-  export default {
+import Tabs from '../components/Tabs_Login.vue';
+export default {
     name: 'Home',
 
     components: {
- 
+      Tabs
     },
-  }
+}
 </script>
