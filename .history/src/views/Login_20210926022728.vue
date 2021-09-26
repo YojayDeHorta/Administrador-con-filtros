4ceb12736c8d0c3c2d7cf6899c901fec9111b059
Ander_AAM <ander_2030@hotmail.com>
2021-09-26 02:41:45 -0500

    Boseto Tablas Falta alinear las letras

---
+++ .history/src/views/Login_20210926022728.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="Marco">
+        <!--
+        <h1 class="Titulo text-center font-weight-bold text-h2 basil--text mt-5">
+            BIENVENIDO<br>
+        </h1>
+        -->
+        <div class="Marco_Login d-flex justify-center">
+            <v-card class="mt-10 mb-10" height="550px" style="width: 450px">
+                <v-hover>
+
+
+                </v-hover>
+               
+            </v-card>
+        </div>
+
+    </div>
+</template>
+<script>
+    export default {
+        name: "Home",
+    };
+</script>
+<style scoped>
+    .Marco_Login {
+        border: 5px solid red;
+
+    }
+</style>
