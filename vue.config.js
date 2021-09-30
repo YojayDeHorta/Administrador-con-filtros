ff149844f9fd1fd66319a1904fcb6e51e81b7fc7
yojay esteban <yojay1000@gmail.com>
2021-09-30 01:25:29 -0500

    añadido icono

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,14 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       builderOptions:{
+        productName: 'Administrador',
         // asar: false,
-        
+        win:{
+          icon: "./public/icono.ico"
+        },
+        directories:{
+          buildResources: "public"
+        }
       },
   
       nodeIntegration: true,
