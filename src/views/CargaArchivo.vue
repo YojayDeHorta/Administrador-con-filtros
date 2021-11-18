b8d0b95e9a5ec9312b6379da33dbfff734738632
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2021-11-17 23:52:39 -0500

    opciones de carga y descarga

--- src/views/CargaArchivo.vue
+++ src/views/CargaArchivo.vue
@@ -1,143 +1,71 @@
 <template>
     <div>
-        <h1>UN ARCHIVO SE CARGA</h1>
+        <v-card>
+            <v-card-title class="text-center justify-center py-6">
+                <h1 class="font-weight-bold text-h5 basil--text">
+                    <strong> PUEDES DESCARGAR / CARGAR UN DOCUMENTO EXCEL</strong>
+                </h1>
+            </v-card-title>
+            <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
+                <v-tab v-for="item in items" :key="item">
+                    {{ item }}
+                </v-tab>
+            </v-tabs>
+            <v-tabs-items v-model="tab">
+                <v-tab-item v-for="item1 in 1" :key="item1">
+                    <v-card class='Tarjeta' color="grey lighten-5" flat>
+                     <h4>Puedes Descargar el Archivo Excel</h4>    <br>
+                        <v-icon color="teal">
+                            mdi-cloud-download
+                        </v-icon>
+                    </v-card>
+                </v-tab-item>
+                <v-tab-item v-for="item2 in 1" :key="item2">
+                    <v-card class='Tarjeta' color="grey lighten-5" flat>
+                        <h4>Sube Tu Archivo</h4>
+                        <v-file-input class='input_tarjeta' hide-input truncate-length="10"></v-file-input>
+                    </v-card>
+                </v-tab-item>
+            </v-tabs-items>
+        </v-card>
     </div>
 </template>
+<script>
+    export default {
+    data () {
+      return {
+        tab: null,
+        items: [
+          'Descargar Excel', 'Subir Excel',
+        ],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      }
+    },
+  }
+</script>
+<style type="text/css" scoped>
+/* Helper classes */
+.basil {
+    background-color: #FFFBE6 !important;
+}
 
+.basil--text {
+    color: #356859 !important;
+}
 
-
-<!--
-<template>
-    <v-toolbar dense>
-        <v-overflow-btn :items="dropdown_font" label="Select font" hide-details class="pa-0"></v-overflow-btn>
-        <template v-if="$vuetify.breakpoint.mdAndUp">
-            <v-divider vertical></v-divider>
-            <v-overflow-btn :items="dropdown_edit" editable label="Select size" hide-details class="pa-0" overflow></v-overflow-btn>
-            <v-divider vertical></v-divider>
-            <v-spacer></v-spacer>
-            <div class="mx-4"></div>
-        </template>
-    </v-toolbar>
-</template>
-<style scoped>
-.Principal {
-    width: 100%;
-    height: 600px;
+.Tarjeta {
+    /*  border: 5px solid red;*/
     margin: auto;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 600px;
+    width: 50%;
+    text-align: center;
+    border: 2px solid grey;
+
 }
 
-.tabla {
-    width: 70%;
-}
-
-.icon_mas {
-    font-size: 250px;
+.input_tarjeta {
+    position: relative;
+    /* border: 5px solid black;*/
+    margin: auto;
+    width: 5%;
 }
 </style>
-<script>
-export default {
-    data() {
-        return {
-            dropdown_font: [{
-                    text: 'NUM SOCIO',
-                },
-                {
-                    text: 'SOCIO',
-                },
-                {
-                    text: 'NUM SOCIO',
-                },
-                {
-                    text: 'NOMBRE',
-                },
-                {
-                    text: 'APELLIDO 1',
-                },
-                {
-                    text: 'APELLIDO 2',
-                },
-                {
-                    text: 'PARENTESCO',
-                },
-                {
-                    text: 'DNI',
-                },
-                {
-                    text: 'PD',
-                },
-                {
-                    text: 'DIRECCION',
-                },
-                {
-                    text: 'COD POSTAL',
-                },
-                {
-                    text: 'LOCALIDAD',
-                },
-                {
-                    text: 'PROVINCIA',
-                },
-                {
-                    text: 'PAIS',
-                },
-                {
-                    text: 'MOVIL',
-                },
-                {
-                    text: 'FIJO',
-                },
-                {
-                    text: 'EMAIL',
-                },
-                {
-                    text: 'EDAD',
-                },
-                {
-                    text: 'SOLA',
-                },
-                {
-                    text: 'MAYOR',
-                },
-                {
-                    text: 'TEFILÁ',
-                },
-                {
-                    text: 'OBSERVACIONES',
-                },
-                {
-                    text: 'OBSERVACIONES',
-                },
-                {
-                    text: 'CERRADO - CUOTAS',
-                },
-                {
-                    text: 'CERRADO - CUOTAS',
-                },
-                {
-                    text: 'CERRADO - FORMA PAGO',
-                },
-                {
-                    text: 'CERRADO - OBSERVACIONES 2',
-                },
-                {
-                    text: 'CERRADO - JESED',
-                },
-            ],
-            dropdown_edit: [
-                { text: '100%' },
-                { text: '75%' },
-                { text: '50%' },
-                { text: '25%' },
-                { text: '0%' },
-            ],
-            toggle_exclusive: 2,
-            toggle_multiple: [1, 2, 3],
-        }
-    },
-}
-</script>
--->
